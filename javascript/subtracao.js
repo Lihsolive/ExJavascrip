20468379264b69d52ae7609e8b5c5352aa37e625
Lihsolive <liliamoliveira13@gmail.com>
2024-09-22 22:49:06 -0300

    feat: creating subtraction function

---
+++ javascript/subtracao.js
@@ -0,0 +1,8 @@
+function subtract() {
+  let n1 = Number(prompt("Digite o primeiro número: "));
+  let n2 = Number(prompt("Digite o segundo número: "));
+  let result = n1 - n2;
+ alert(`${n1} - ${n2} = ${result}`);
+}
+
+subtract();
